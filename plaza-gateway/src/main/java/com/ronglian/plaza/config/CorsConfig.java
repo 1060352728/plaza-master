17e3fde6ce63271db8ae8ac07de27b1e773dac8f
1060352728 <43266944+1060352728@users.noreply.github.com>
2018-12-25 17:33:28 +0800

    使用cors解决前台访问跨域问题

--- plaza-gateway/src/main/java/com/ronglian/plaza/config/CorsConfig.java
+++ plaza-gateway/src/main/java/com/ronglian/plaza/config/CorsConfig.java
@@ -6,8 +6,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-
 /**
  * @author likui
  * @Classname: CorsConfig
@@ -18,17 +16,27 @@ import java.util.Arrays;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter(){
+    public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);//允许cookie跨域
-        config.setAllowedOrigins(Arrays.asList("*"));//原始域，自己定义的域名
-        config.setAllowedHeaders(Arrays.asList("*"));//允许的头
-        config.setAllowedMethods(Arrays.asList("*"));//允许的get和post请求
-        config.setMaxAge(200l);//相同的请求200秒之内不会再去查
-
-        source.registerCorsConfiguration("/**",config);
+        // 允许cookies跨域
+        config.setAllowCredentials(true);
+        // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        config.addAllowedOrigin("*");
+        // #允许访问的头信息,*表示全部
+        config.addAllowedHeader("*");
+        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        config.setMaxAge(18000L);
+        // 允许提交请求的方法，*表示全部允许
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        // 允许Get的请求方法
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }

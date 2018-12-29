b705386ed5203240c40e99362ce2f38695be4ef0
likui <1060352728@qq.com>
2018-12-29 23:39:17 +0800

    解决跨域OPTIONS传递Authorization = "Bearer "无法通过的问题

--- plaza-gateway/src/main/java/com/ronglian/plaza/config/WebSecurityConfig.java
+++ plaza-gateway/src/main/java/com/ronglian/plaza/config/WebSecurityConfig.java
@@ -2,17 +2,26 @@ package com.ronglian.plaza.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableOAuth2Sso
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(-1)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.requestMatchers()
+                .antMatchers(HttpMethod.OPTIONS, "/oauth/token", "/rest/**", "/api/**", "/**")
+                .and()
+                .csrf().disable();
     }
 
 }

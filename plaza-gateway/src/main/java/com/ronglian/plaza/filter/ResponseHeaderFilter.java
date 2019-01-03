559b01932490cb02d7dfbb12250576f1904f8487
likui <1060352728@qq.com>
2019-01-03 20:28:04 +0800

    zuul过滤器判断是否为获取access_token，如果是但响应码不是200，修改响应码并提示

--- plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
+++ plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
@@ -6,18 +6,10 @@ import com.netflix.zuul.context.RequestContext;
 import com.ronglian.plaza.util.common.JsonUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Map;
-import java.util.UUID;
-
 import static com.netflix.zuul.context.RequestContext.getCurrentContext;
-import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
-import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
-import static org.springframework.util.ReflectionUtils.rethrowRuntimeException;
 
 /**
  * 过滤器响应头中添加信息
@@ -40,16 +32,16 @@ public class ResponseHeaderFilter extends ZuulFilter{
     public Object run() {
         try {
             RequestContext context = getCurrentContext();
-            InputStream stream = context.getResponseDataStream();
-            String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
-            Map<String, String> map = (Map<String, String>) JsonUtil.fromJson(body,new TypeReference<Map<String, String>>() {});
-            Integer code = context.getResponseStatusCode();
             String url = context.getRequest().getRequestURI();
+            Integer code = context.getResponseStatusCode();
             if(url.indexOf("/oquth/token") < 0 && code != 200){
+                InputStream stream = context.getResponseDataStream();
+                String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+                Map<String, String> map = (Map<String, String>) JsonUtil.fromJson(body,new TypeReference<Map<String, String>>() {});
                 map.put("code",code.toString());
                 context.setResponseStatusCode(200);
+                context.setResponseBody(map.toString());
             }
-            context.setResponseBody(map.toString());
         }
         catch (Exception e) {
 

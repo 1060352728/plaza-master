31485bab7caa58372061de2fd1f3abcbe4d2d5dd
likui <1060352728@qq.com>
2019-01-03 20:16:49 +0800

    zuul过滤器判断是否为获取access_token，如果是但响应码不是200，修改响应码并提示

--- plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
+++ plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
@@ -1,38 +1,59 @@
 package com.ronglian.plaza.filter;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import com.ronglian.plaza.util.common.JsonUtil;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Map;
 import java.util.UUID;
+
+import static com.netflix.zuul.context.RequestContext.getCurrentContext;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
+import static org.springframework.util.ReflectionUtils.rethrowRuntimeException;
 
 /**
  * 过滤器响应头中添加信息
  */
 @Component
 public class ResponseHeaderFilter extends ZuulFilter{
-    @Override
+
     public String filterType() {
-        return POST_TYPE;
+        return "post";
     }
 
-    @Override
     public int filterOrder() {
-        return SEND_RESPONSE_FILTER_ORDER - 1;
+        return 999;
     }
 
-    @Override
     public boolean shouldFilter() {
         return true;
     }
 
-    @Override
     public Object run() {
-        RequestContext requestContext = RequestContext.getCurrentContext();
-        HttpServletResponse response = requestContext.getResponse();
-        response.setHeader("X-Foo", UUID.randomUUID().toString());
+        try {
+            RequestContext context = getCurrentContext();
+            InputStream stream = context.getResponseDataStream();
+            String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+            Map<String, String> map = (Map<String, String>) JsonUtil.fromJson(body,new TypeReference<Map<String, String>>() {});
+            Integer code = context.getResponseStatusCode();
+            String url = context.getRequest().getRequestURI();
+            if(url.indexOf("/oquth/token") < 0 && code != 200){
+                map.put("code",code.toString());
+                context.setResponseStatusCode(200);
+            }
+            context.setResponseBody(map.toString());
+        }
+        catch (Exception e) {
+
+        }
         return null;
     }
 }

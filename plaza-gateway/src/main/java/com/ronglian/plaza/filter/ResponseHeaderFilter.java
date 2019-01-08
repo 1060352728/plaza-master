0ff7978c6cedab8f21060d384945bc3ed5e39740
likui <1060352728@qq.com>
2019-01-08 22:36:14 +0800

    精确提示登录错误信息

--- plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
+++ plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
@@ -1,15 +1,9 @@
 package com.ronglian.plaza.filter;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.ronglian.plaza.util.common.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.util.Map;
 import static com.netflix.zuul.context.RequestContext.getCurrentContext;
 
 /**
@@ -36,17 +30,13 @@ public class ResponseHeaderFilter extends ZuulFilter{
             RequestContext context = getCurrentContext();
             String url = context.getRequest().getRequestURI();
             Integer code = context.getResponseStatusCode();
-            if(url.indexOf("/oauth/token") > 0 && code == 401){
-                InputStream stream = context.getResponseDataStream();
-                String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
-                Map<String, String> map = (Map<String, String>) JsonUtil.fromJson(body,new TypeReference<Map<String, String>>() {});
-                map.put("code",code.toString());
+            if(url.indexOf("/oauth/token") > 0 && code != 200){
                 context.setResponseStatusCode(200);
-                context.setResponseBody(map.toString());
+                context.setResponseBody(code.toString());
             }
         }
         catch (Exception e) {
-            log.error("登录出错",e);
+            log.error("登录出错");
         }
         return null;
     }

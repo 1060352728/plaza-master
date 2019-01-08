2b884d9589cbc77e65f405c0d2255b9980623d2b
likui <1060352728@qq.com>
2019-01-08 22:21:56 +0800

    修改登录之后的拦截器

--- plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
+++ plaza-gateway/src/main/java/com/ronglian/plaza/filter/ResponseHeaderFilter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.ronglian.plaza.util.common.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 import java.io.InputStream;
@@ -15,6 +16,7 @@ import static com.netflix.zuul.context.RequestContext.getCurrentContext;
  * 过滤器响应头中添加信息
  */
 @Component
+@Slf4j
 public class ResponseHeaderFilter extends ZuulFilter{
 
     public String filterType() {
@@ -34,7 +36,7 @@ public class ResponseHeaderFilter extends ZuulFilter{
             RequestContext context = getCurrentContext();
             String url = context.getRequest().getRequestURI();
             Integer code = context.getResponseStatusCode();
-            if(url.indexOf("/oquth/token") < 0 && code != 200){
+            if(url.indexOf("/oauth/token") > 0 && code == 401){
                 InputStream stream = context.getResponseDataStream();
                 String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
                 Map<String, String> map = (Map<String, String>) JsonUtil.fromJson(body,new TypeReference<Map<String, String>>() {});
@@ -44,7 +46,7 @@ public class ResponseHeaderFilter extends ZuulFilter{
             }
         }
         catch (Exception e) {
-
+            log.error("登录出错",e);
         }
         return null;
     }
